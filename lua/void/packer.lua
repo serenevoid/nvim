4b3e59aedcd64d2885986c471809ecb615ebdf32
Paranoid Wolf <ajuph9224@gmail.com>
2022-07-01 14:14:23 +0530

    lsp config todo

--- lua/void/packer.lua
+++ lua/void/packer.lua
@@ -1,29 +1,41 @@
 return require('packer').startup(function()
-  -- Packer can manage itself
-  use('wbthomason/packer.nvim')
+    -- packer can manage itself
+    use('wbthomason/packer.nvim')
 
-  -- Neogit
-  use('TimUntersberger/neogit')
+    -- Neogit
+    use('TimUntersberger/neogit')
 
-  -- Undotree
-  use('jiaoshijie/undotree')
+    -- Undotree
+    use('jiaoshijie/undotree')
 
-  -- Common Functions library
-  use('nvim-lua/plenary.nvim')
+    -- Common Functions library
+    use('nvim-lua/plenary.nvim')
+    use("nvim-lua/popup.nvim")
 
-  -- Telescope
-  use('nvim-telescope/telescope.nvim')
+    -- LSP stuff
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/nvim-cmp")
+    use("onsails/lspkind-nvim")
+    use("glepnir/lspsaga.nvim")
+    use("simrat39/symbols-outline.nvim")
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
 
-  -- Treesitter
-  use('nvim-treesitter/nvim-treesitter', {
-	  run = ':TSUpdate'
-  })
-  use('nvim-treesitter/nvim-treesitter-context')
+    -- Telescope
+    use('nvim-telescope/telescope.nvim')
 
-  -- Harpoon
-  use('ThePrimeagen/harpoon')
+    -- Treesitter
+    use('nvim-treesitter/nvim-treesitter', {
+      run = ':TSUpdate'
+    })
+    use('nvim-treesitter/nvim-treesitter-context')
 
-  -- Colorscheme
-  use('folke/tokyonight.nvim')
-  use('gruvbox-community/gruvbox')
+    -- Harpoon
+    use('ThePrimeagen/harpoon')
+
+    -- Colorscheme
+    use('folke/tokyonight.nvim')
+    use('gruvbox-community/gruvbox')
 end)
