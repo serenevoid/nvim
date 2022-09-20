e1ce30bde0021c24a59ee2b2d6a4db937478eee3
Paranoid Wolf <ajuph9224@gmail.com>
2022-09-21 00:06:54 +0530

    changed harpoon keymaps

--- after/plugin/keymaps/harpoon.lua
+++ after/plugin/keymaps/harpoon.lua
@@ -2,11 +2,11 @@ local nnoremap = require("void.keymap").nnoremap
 
 local silent = { silent = true }
 
-nnoremap("<leader>a", ":lua require(\"harpoon.mark\").add_file()<CR>", silent)
-nnoremap("<C-e>", ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>", silent)
-nnoremap("<leader>tc", ":lua require(\"harpoon.cmd-ui\").toggle_quick_menu()<CR>", silent)
+nnoremap("<leader>ha", ":lua require(\"harpoon.mark\").add_file()<CR>", silent)
+nnoremap("<leader>hh", ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>", silent)
+nnoremap("<leader>ht", ":lua require(\"harpoon.cmd-ui\").toggle_quick_menu()<CR>", silent)
 
-nnoremap("<C-h>", ":lua require(\"harpoon.ui\").nav_file(1)<CR>", silent)
-nnoremap("<C-t>", ":lua require(\"harpoon.ui\").nav_file(2)<CR>", silent)
-nnoremap("<C-n>", ":lua require(\"harpoon.ui\").nav_file(3)<CR>", silent)
-nnoremap("<C-s>", ":lua require(\"harpoon.ui\").nav_file(4)<CR>", silent)
+nnoremap("<leader>1", ":lua require(\"harpoon.ui\").nav_file(1)<CR>", silent)
+nnoremap("<leader>2", ":lua require(\"harpoon.ui\").nav_file(2)<CR>", silent)
+nnoremap("<leader>3", ":lua require(\"harpoon.ui\").nav_file(3)<CR>", silent)
+nnoremap("<leader>4", ":lua require(\"harpoon.ui\").nav_file(4)<CR>", silent)
