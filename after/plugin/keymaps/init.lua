eb9951731fc5d29f0c7258af95c1e0a8fcee527e
Paranoid Wolf <ajuph9224@gmail.com>
2022-10-14 10:09:09 +0000

    removed unused keymaps

--- after/plugin/keymaps/init.lua
+++ after/plugin/keymaps/init.lua
@@ -2,8 +2,6 @@ local Remap = require("void.keymap")
 local nnoremap = Remap.nnoremap
 local vnoremap = Remap.vnoremap
 local inoremap = Remap.inoremap
-local xnoremap = Remap.xnoremap
-local nmap = Remap.nmap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
@@ -19,13 +17,9 @@ nnoremap("J", "mzJ`z")
 nnoremap("<C-d>", "<C-d>zz")
 nnoremap("<C-u>", "<C-u>zz")
 
--- greatest remap ever
-xnoremap("<leader>p", "\"_dP")
-
--- next greatest remap ever : asbjornHaland
 nnoremap("<leader>y", "\"+y")
 vnoremap("<leader>y", "\"+y")
-nmap("<leader>Y", "\"+Y")
+nnoremap("<leader>Y", "\"+Y")
 
 nnoremap("<leader>d", "\"_d")
 vnoremap("<leader>d", "\"_d")
@@ -41,6 +35,3 @@ nnoremap("<C-k>", "<cmd>cnext<CR>zz")
 nnoremap("<C-j>", "<cmd>cprev<CR>zz")
 nnoremap("<leader>k", "<cmd>lnext<CR>zz")
 nnoremap("<leader>j", "<cmd>lprev<CR>zz")
-
-nnoremap("<leader>s", "<cmd>%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
