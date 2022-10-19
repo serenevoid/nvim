1edb419ce03f7c2037c08df1e886410b7377be08
Paranoid Wolf <ajuph9224@gmail.com>
2022-10-19 14:31:06 +0000

    chore: refactoring

--- lua/void/wiki.lua
+++ lua/void/wiki.lua
@@ -10,14 +10,7 @@ wiki.create_diary_entry = function()
     local diary_name = string.format(diary_path .. sep .. "%s.md", os.date "%Y_%m_%d")
     vim.fn.mkdir(diary_path, "p")
     local bufnr = vim.fn.bufnr(diary_name, true)
-    for _, win_id in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-        local open_bufnr = vim.api.nvim_win_get_buf(win_id)
-        if open_bufnr == bufnr then
-            return vim.api.nvim_set_current_win(win_id)
-        end
-    end
-
-    vim.api.nvim_win_set_buf(0, bufnr)
+    Open_buffer(bufnr)
 end
 
 wiki.open_index = function()
@@ -27,15 +20,8 @@ wiki.open_index = function()
     local journal_dir = home .. sep .. "wiki" .. sep .. "journal"
     vim.fn.mkdir(journal_dir, "p")
     local bufnr = vim.fn.bufnr(index_path, true)
-    vim.api.nvim_buf_set_keymap(bufnr, "v", "<CR>", ":'<,'>lua require(\"void.wiki\").create_wiki_file()<CR>", {})
-    for _, win_id in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-        local open_bufnr = vim.api.nvim_win_get_buf(win_id)
-        if open_bufnr == bufnr then
-            return vim.api.nvim_set_current_win(win_id)
-        end
-    end
-
-    vim.api.nvim_win_set_buf(0, bufnr)
+    Set_keymaps(bufnr)
+    Open_buffer(bufnr)
 end
 
 wiki.create_wiki_file = function()
@@ -51,6 +37,11 @@ wiki.create_wiki_file = function()
     vim.api.nvim_set_current_line(nline)
     local journal_dir = home .. sep .. "wiki" .. sep .. "journal"
     local bufnr = vim.fn.bufnr(journal_dir .. sep .. filename, true)
+    Set_keymaps(bufnr)
+    Open_buffer(bufnr)
+end
+
+Set_keymaps = function(bufnr)
     vim.api.nvim_buf_set_keymap(
         bufnr,
         "v",
@@ -61,6 +52,9 @@ wiki.create_wiki_file = function()
             silent = true,
             nowait = true,
         })
+end
+
+Open_buffer = function(bufnr)
     for _, win_id in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
         local open_bufnr = vim.api.nvim_win_get_buf(win_id)
         if open_bufnr == bufnr then
