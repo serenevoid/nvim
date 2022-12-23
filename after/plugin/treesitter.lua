7f4c343e6f8618fe8f2c43747ca557947a4e7f98
Paranoid Wolf <ajuph9224@gmail.com>
2022-12-24 01:27:40 +0530

    chore: added treesitter languages

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "go"},
+  ensure_installed = { "c", "lua", "rust", "go", "typescript", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  ignore_install = {},
 
   highlight = {
     -- `false` will disable the whole extension
@@ -16,7 +16,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "c", "rust" },
+    disable = { },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
