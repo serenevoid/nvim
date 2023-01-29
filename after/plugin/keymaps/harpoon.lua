7c4ead65518759ec96ae11d6c5eebe8c8137941f
Paranoid Wolf <ajuph9224@gmail.com>
2023-01-29 19:07:42 +0530

    feat: added Align comman

--- after/plugin/keymaps/harpoon.lua
+++ after/plugin/keymaps/harpoon.lua
@@ -2,8 +2,8 @@ local nnoremap = require("void.keymap").nnoremap
 
 local silent = { silent = true }
 
-nnoremap("<leader>ha", ":lua require(\"harpoon.mark\").add_file()<CR>", silent)
-nnoremap("<leader>hh", ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>", silent)
+nnoremap("<leader>ha", ":lua require(\"harpoon.mark\").add_file()<CR>",            silent)
+nnoremap("<leader>hh", ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>",     silent)
 nnoremap("<leader>ht", ":lua require(\"harpoon.cmd-ui\").toggle_quick_menu()<CR>", silent)
 
 nnoremap("<leader>h1", ":lua require(\"harpoon.ui\").nav_file(1)<CR>", silent)
