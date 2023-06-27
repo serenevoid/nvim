8d7a2da9266d31b6534abf6845fb9056216463ab
Serene Void <ajuph9224@gmail.com>
2023-06-27 23:42:27 +0530

    chore(set.lua): reduced tabs to 2

--- lua/void/set.lua
+++ lua/void/set.lua
@@ -2,9 +2,9 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.hidden = true
 vim.opt.errorbells = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
@@ -13,7 +13,7 @@ vim.opt.backup = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 4
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
