6b0e1736a523438b11e03484da19519becc9f18f
Serene Void <ajuph9224@gmail.com>
2023-07-11 19:18:08 +0530

    fix(treesitter): help changed to vimdoc

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "c", "python", "lua", "rust", "go", "typescript", "javascript" },
+  ensure_installed = { "vimdoc", "c", "python", "lua", "rust", "go", "typescript", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
