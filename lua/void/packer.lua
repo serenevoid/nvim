13513cc97dd040eef3749a7700a2130d28b3b7d8
SereneVoid <ajuph9224@gmail.com>
2023-07-13 12:13:01 +0530

    chore(packer): added local plugin to list

--- lua/void/packer.lua
+++ lua/void/packer.lua
@@ -53,4 +53,6 @@ return require('packer').startup(function(use)
     use('folke/tokyonight.nvim')
     use('kyazdani42/nvim-web-devicons')
 
+    -- Wiki
+    use('$HOME/dev/projects/kiwi.nvim')
 end)
