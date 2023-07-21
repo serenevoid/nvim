9f3d61aac74d8b63ecad0d4a2b4da641daff4ba6
serenevoid <ajuph9224@gmail.com>
2023-07-21 22:27:48 +0530

    fix(kiwi): resolved keymap conflict and added new changes

--- after/plugin/keymaps/kiwi.lua
+++ after/plugin/keymaps/kiwi.lua
@@ -1,7 +1,6 @@
-local Remap = require("void.keymap")
-local nnoremap = Remap.nnoremap
-local kiwi = require("kiwi")
-
-nnoremap("<leader>ww", kiwi.open_wiki_index)
-nnoremap("<leader>wd", kiwi.open_diary_index)
-nnoremap("<leader>wn", kiwi.open_diary_new)
+local nnoremap = require("void.keymap").nnoremap
+local kiwi = require('kiwi')
+-- Necessary keybindings
+nnoremap('<leader>ww', kiwi.open_wiki_index)
+nnoremap('<leader>wd', kiwi.open_diary_index)
+nnoremap('<leader>wn', kiwi.open_diary_new)
