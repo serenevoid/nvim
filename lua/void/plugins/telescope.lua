87ff5fdb63528b3583a43ad5408d156c5f495294
serenevoid <ajuph9224@gmail.com>
2023-09-04 10:05:05 +0530

    fix(telescope): grep_string not taking search value

--- lua/void/plugins/telescope.lua
+++ lua/void/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
 			{ "<leader>pg", "<cmd>Telescope git_files<cr>", desc = "Find git tracked files" },
 			{ "<leader>pb", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
 			{ "<leader>ph", "<cmd>Telescope help_tags<cr>", desc = "Search Help Tags" },
-			{ "<leader>ps", "<cmd>Telescope grep_string({ search = vim.fn.input(\"Grep For > \")})<cr>", desc = "Grep String" }
+			{ "<leader>ps", "<cmd>Telescope grep_string<cr>", desc = "Grep String" }
 	},
 	lazy = true
 }
