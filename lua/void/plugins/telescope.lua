a563c69999957e54f75b5907afa32f32226c2996
serenevoid <ajuph9224@gmail.com>
2023-09-07 11:53:46 +0530

    feat(telescope): added new keybindings

--- lua/void/plugins/telescope.lua
+++ lua/void/plugins/telescope.lua
@@ -9,7 +9,14 @@ return {
 			{ "<leader>pg", "<cmd>Telescope git_files<cr>", desc = "Find git tracked files" },
 			{ "<leader>pb", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
 			{ "<leader>ph", "<cmd>Telescope help_tags<cr>", desc = "Search Help Tags" },
-			{ "<leader>ps", "<cmd>Telescope grep_string<cr>", desc = "Grep String" }
-	},
-	lazy = true
+			{
+				"<leader>ps",
+				":lua require(\"telescope.builtin\").grep_string({ search = vim.fn.input(\"Grep > \") })<cr>",
+				desc = "Grep String"
+			},
+			{ "<leader>p:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+			{ "<leader>pd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
+			{ "<leader>pD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" }
+		},
+		lazy = true
 }
