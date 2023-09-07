6785ae2d350d69b9235cd777776df73a928cbf36
serenevoid <ajuph9224@gmail.com>
2023-09-07 13:29:42 +0530

    chore(keymaps): git and harpoon keymap changes

--- lua/void/plugins/git.lua
+++ lua/void/plugins/git.lua
@@ -11,9 +11,9 @@ return ({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>[h', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
-        vim.keymap.set('n', '<leader>]h', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview Hunk' })
+        vim.keymap.set('n', '<leader>gk', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
+        vim.keymap.set('n', '<leader>gj', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview Hunk' })
       end,
     },
 })
--- lua/void/plugins/harpoon.lua
+++ lua/void/plugins/harpoon.lua
@@ -2,7 +2,6 @@ return({
 	'ThePrimeagen/harpoon',
 	keys = {
 			{ "<leader>ha", ":lua require(\"harpoon.mark\").add_file()<cr>", desc = "Add file to harpoon" },
-			{ "<leader>hh", ":lua require(\"harpoon.ui\").toggle_quick_menu()<cr>", desc = "Launch harpoon " },
-			{ "<leader>ht", ":lua require(\"harpoon.cmd-ui\").toggle_quick_menu()<cr>", desc = "Launch harpoon command" }
+			{ "<leader>hh", ":lua require(\"harpoon.ui\").toggle_quick_menu()<cr>", desc = "Launch harpoon " }
 	}
 })
