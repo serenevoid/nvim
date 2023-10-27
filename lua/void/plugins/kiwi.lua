11d7c3fa3b395ed65c0939e5876b852af04d7a5b
serenevoid <ajuph9224@gmail.com>
2023-10-27 21:25:03 +0530

    chore(kiwi): removed diary keybindings

--- lua/void/plugins/kiwi.lua
+++ lua/void/plugins/kiwi.lua
@@ -6,8 +6,6 @@ return ({
     },
     keys = {
       { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
-      { "<leader>wd", ":lua require(\"kiwi\").open_diary_index()<cr>", desc = "Open Diary index" },
-      { "<leader>wn", ":lua require(\"kiwi\").open_diary_new()<cr>", desc = "Open today's Diary" },
       { "<leader>t", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
     },
     lazy = true
