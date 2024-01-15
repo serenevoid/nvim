39fedb57d2c17179006a890f291ed2a2e86b4d68
serenevoid <ajuph9224@gmail.com>
2024-01-15 08:47:08 +0530

    chore(snippet): changed keybind to choice toggle

--- lua/void/plugins/snippet.lua
+++ lua/void/plugins/snippet.lua
@@ -8,7 +8,7 @@ return {
       vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
-      vim.keymap.set({"i", "s"}, "<C-E>", function()
+      vim.keymap.set({"i", "s"}, "<C-;>", function()
         if ls.choice_active() then
           ls.change_choice(1)
         end
