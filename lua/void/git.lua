ca9c62bb97452c5ddafc260538e5e4292875727d
Void <41177205+serenevoid@users.noreply.github.com>
2024-01-16 13:44:20 +0530

    fix(git): git blame line number fix

--- lua/void/git.lua
+++ lua/void/git.lua
@@ -1,7 +1,7 @@
 function Blame_line()
   local bufnr = vim.api.nvim_get_current_buf()
   local filename = vim.api.nvim_buf_get_name(bufnr)
-  local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+  local row = vim.api.nvim_win_get_cursor(0)[1]
   local blame_info = vim.fn.systemlist('git blame -L ' .. row .. ',+1 ' .. filename .. ' --porcelain')
   if blame_info[2] ~= nil then
     local hash = string.sub(blame_info[1], 1, 8)
