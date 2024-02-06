a0e42a8000747ca5b2792553efc0d40334667417
serenevoid <ajuph9224@gmail.com>
2024-02-06 09:39:02 +0530

    fix(kiwi): removed toggle keymap

--- lua/void/plugins/kiwi.lua
+++ lua/void/plugins/kiwi.lua
@@ -6,7 +6,6 @@ return ({
     },
     keys = {
       { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
-      { "<leader>t", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
     },
     lazy = true
   }
