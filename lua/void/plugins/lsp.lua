586e5c4d54888f5797d3cf79b7105ea461f51e04
serenevoid <ajuph9224@gmail.com>
2024-07-04 14:51:51 +0530

    fix(lsp): lua_ls settings update

--- lua/void/plugins/lsp.lua
+++ lua/void/plugins/lsp.lua
@@ -27,18 +27,23 @@ return {
             require("lspconfig").lua_ls.setup({
               settings = {
                 Lua = {
+                  runtime = {
+                    version = 'LuaJIT',
+                  },
                   diagnostics = {
-                    globals = { "vim" }
+                    enable = true,
+                    globals = {'vim', 'use'},
                   },
                   workspace = {
                     checkThirdParty = false,
                     telemetry = { enable = false },
                     library = {
-                      "${3rd}/love2d/library"
-                    }
-                  }
-                }
-              }
+                      "${3rd}/love2d/library",
+                      vim.api.nvim_get_runtime_file('', true),
+                    },
+                  },
+                },
+              },
             })
           end
         }
