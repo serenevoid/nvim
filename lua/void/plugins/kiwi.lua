4086a6fff40949be18f877232cfcbc807ad44027
serenevoid <ajuph9224@gmail.com>
2024-12-12 13:54:05 +0530

    fix(kiwi): adjust according to lazy

--- lua/void/plugins/kiwi.lua
+++ lua/void/plugins/kiwi.lua
@@ -1,12 +1,15 @@
 return ({
   {
-    dir = '$HOME/dev/kiwi.nvim',
+    dir = '~/dev/kiwi.nvim',
+    url = 'serenevoid/kiwi.nvim',
     dependencies = {
       "nvim-lua/plenary.nvim"
     },
     keys = {
       { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
+      { "<leader>tt", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
     },
+    dev = true,
     lazy = true
   }
 })
