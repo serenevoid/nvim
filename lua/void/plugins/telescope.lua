8949ab36589e4bdecdc235e309e7b2dcc656ec99
serenevoid <ajuph9224@gmail.com>
2024-12-12 17:10:36 +0530

    fix(telescope): fzf-native introduction and new keymap

--- lua/void/plugins/telescope.lua
+++ lua/void/plugins/telescope.lua
@@ -2,21 +2,33 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-fzf-native.nvim'
+  },
+  extensions = {
+    fzf = {}
   },
   keys = {
-    { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    { "<leader>pg", "<cmd>Telescope git_files<cr>", desc = "Find git tracked files" },
-    { "<leader>pb", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
-    { "<leader>ph", "<cmd>Telescope help_tags<cr>", desc = "Search Help Tags" },
+    { "<leader>pf", require("telescope.builtin").find_files, desc = "Find files" },
+    { "<leader>pg", require("telescope.builtin").git_files, desc = "Find git tracked files" },
+    { "<leader>pb", require("telescope.builtin").buffers, desc = "Find Buffer" },
+    { "<leader>ph", require("telescope.builtin").help_tags, desc = "Search Help Tags" },
+    { "<leader>p:", require("telescope.builtin").command_history, desc = "Command History" },
+    { "<leader>pD", require("telescope.builtin").diagnostics, desc = "Workspace diagnostics" },
     {
       "<leader>ps",
-      ":lua require(\"telescope.builtin\").grep_string({ search = vim.fn.input(\"Grep > \") })<cr>",
+      function ()
+        require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+      end,
       desc = "Grep String"
     },
-    { "<leader>p:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader>pd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
-    { "<leader>pD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" }
+    {
+      "<leader>en",
+      function ()
+        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Edit neovim"
+    }
   },
   lazy = true
 }
