063ba2f141ec623f1258a24485d0d55693eefddd
serenevoid <ajuph9224@gmail.com>
2024-12-17 18:40:48 +0530

    chore(kiwi+telescope):

--- lua/void/plugins/kiwi.lua
+++ lua/void/plugins/kiwi.lua
@@ -1,15 +1,10 @@
-return ({
-  {
-    dir = '~/dev/kiwi.nvim',
-    url = 'serenevoid/kiwi.nvim',
-    dependencies = {
-      "nvim-lua/plenary.nvim"
-    },
-    keys = {
-      { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
-      { "<leader>tt", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
-    },
-    dev = true,
-    lazy = true
-  }
-})
+return {
+  dir = '~/dev/kiwi.nvim',
+  url = 'serenevoid/kiwi.nvim',
+  keys = {
+    { '<leader>ww', ':lua require(\'kiwi\').open_wiki_index()<cr>', desc = 'Open Wiki index' },
+    { '<leader>tt', ':lua require(\'kiwi\').todo.toggle()<cr>', desc = 'Toggle Markdown Task' }
+  },
+  dev = true,
+  lazy = true
+}
--- lua/void/plugins/telescope.lua
+++ lua/void/plugins/telescope.lua
@@ -9,25 +9,25 @@ return {
     fzf = {}
   },
   keys = {
-    { "<leader>pf", require("telescope.builtin").find_files, desc = "Find files" },
-    { "<leader>pg", require("telescope.builtin").git_files, desc = "Find git tracked files" },
-    { "<leader>pb", require("telescope.builtin").buffers, desc = "Find Buffer" },
-    { "<leader>ph", require("telescope.builtin").help_tags, desc = "Search Help Tags" },
-    { "<leader>p:", require("telescope.builtin").command_history, desc = "Command History" },
-    { "<leader>pD", require("telescope.builtin").diagnostics, desc = "Workspace diagnostics" },
+    { '<leader>pf', require('telescope.builtin').find_files, desc = 'Find files' },
+    { '<leader>pg', require('telescope.builtin').git_files, desc = 'Find git tracked files' },
+    { '<leader>pb', require('telescope.builtin').buffers, desc = 'Find Buffer' },
+    { '<leader>ph', require('telescope.builtin').help_tags, desc = 'Search Help Tags' },
+    { '<leader>p:', require('telescope.builtin').command_history, desc = 'Command History' },
+    { '<leader>pD', require('telescope.builtin').diagnostics, desc = 'Workspace diagnostics' },
     {
-      "<leader>ps",
+      '<leader>ps',
       function ()
-        require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+        require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })
       end,
-      desc = "Grep String"
+      desc = 'Grep String'
     },
     {
-      "<leader>en",
+      '<leader>en',
       function ()
-        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+        require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })
       end,
-      desc = "Edit neovim"
+      desc = 'Edit neovim'
     }
   },
   lazy = true
