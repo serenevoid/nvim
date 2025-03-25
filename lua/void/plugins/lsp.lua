60a417e7b7e8c331006954cbe10f4da351116eeb
serenevoid <ajuph9224@gmail.com>
2025-03-25 14:58:21 +0530

    feat(lsp): add jump to built-in lsp

--- lua/void/plugins/lsp.lua
+++ lua/void/plugins/lsp.lua
@@ -47,7 +47,7 @@ return {
         }
       })
 
-      local cmp = require'cmp'
+      local cmp = require('cmp')
 
       cmp.setup({
         snippet = {
@@ -59,13 +59,48 @@ return {
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true })
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(
+            function(fallback)
+              if vim.snippet.active({direction = 1}) then
+                vim.snippet.jump(1)
+              else
+                fallback()
+              end
+            end, {'i', 's'}
+          ),
+          ['<S-Tab>'] = cmp.mapping(
+            function(fallback)
+              if vim.snippet.active({direction = -1}) then
+                vim.snippet.jump(-1)
+              else
+                fallback()
+              end
+            end, {'i', 's'}
+          )
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
         }, {
             { name = 'buffer' },
           })
+      })
+
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+          }),
+        matching = { disallow_symbol_nonprefix_matching = false }
       })
     end
   },
