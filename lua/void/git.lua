2cb649831aa21dc98f7c88fdf35ca6fef38a2775
serenevoid <ajuph9224@gmail.com>
2025-03-28 15:43:43 +0530

    refactor(git): better blames

--- lua/void/git.lua
+++ lua/void/git.lua
@@ -1,18 +1,30 @@
-local M = {}
+local M = {
+  nsId = vim.api.nvim_create_namespace('git'),
+  extId = nil,
+  bufnr = nil
+}
 
 function M.blame_line()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local filename = vim.api.nvim_buf_get_name(bufnr)
+  if M.extId ~= nil then
+    vim.api.nvim_buf_del_extmark(M.bufnr, M.nsId, M.extId)
+  end
+  M.bufnr = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(M.bufnr)
   local row = vim.api.nvim_win_get_cursor(0)[1]
   local blame_info = vim.fn.systemlist('git blame -L ' .. row .. ',+1 ' .. filename .. ' --porcelain')
   if blame_info[2] ~= nil then
     local hash = string.sub(blame_info[1], 1, 8)
     local author_name = string.sub(blame_info[2], 8)
     local author_date = os.date('%Y %b %d', tonumber(string.sub(blame_info[4], 12)))
-    local summary = string.sub(blame_info[10], 9)
-    print(hash .. " - " .. author_name .. " - " .. author_date .. " - " ..  summary)
+    M.extId = vim.api.nvim_buf_set_extmark(M.bufnr, M.nsId, row - 1, 0, {
+      virt_text = { { hash, 1 }, { " - ", 2 }, { author_name, 0 }, { " - ", 2 }, { author_date, 1 } },
+      virt_text_pos = "right_align"
+    })
+    vim.defer_fn(function ()
+      vim.api.nvim_buf_del_extmark(M.bufnr, M.nsId, M.extId)
+    end, 10000)
   else
-    print(blame_info[1])
+    vim.notify(blame_info[1], vim.log.levels.ERROR)
   end
 end
 
