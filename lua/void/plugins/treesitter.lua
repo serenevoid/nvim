1044ffc6b9ce0cfc3d5b54e3f9bc5343cb0b621d
SereneVoid <ajuph9224@gmail.com>
2025-06-19 05:33:21 +0530

    fix(treesitter); Prevent building before startup completes

--- lua/void/plugins/treesitter.lua
+++ lua/void/plugins/treesitter.lua
@@ -2,36 +2,22 @@ return({
   {
     "nvim-treesitter/nvim-treesitter",
     version = false,
-    build = ":TSUpdate",
     event = { "VeryLazy" },
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
       ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
+        "bash", "c", "html", "javascript", "jsdoc", "json", "jsonc", "lua",
+        "luadoc", "luap", "markdown", "markdown_inline", "python", "regex",
+        "toml", "tsx", "typescript", "vim", "vimdoc", "yaml",
       },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
+
+      vim.defer_fn(function()
+        pcall(vim.cmd, "TSUpdate")
+      end, 0)
     end
   },
 })
