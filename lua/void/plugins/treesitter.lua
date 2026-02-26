d9e445a9d8162303f2fa2ab5c10da076f3b23bad
P H Aju <aju.ph@exorint.com>
2026-02-26 12:05:26 +0000

    fix(treesitter): updated config for latest version

--- lua/void/plugins/treesitter.lua
+++ lua/void/plugins/treesitter.lua
@@ -1,23 +1,19 @@
-return({
+return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false,
-    event = { "VeryLazy" },
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
       ensure_installed = {
-        "bash", "c", "html", "javascript", "jsdoc", "json", "jsonc", "lua",
-        "luadoc", "luap", "markdown", "markdown_inline", "python", "regex",
-        "toml", "tsx", "typescript", "vim", "vimdoc", "yaml",
+        "bash", "c", "html", "javascript", "jsdoc", "json", "jsonc",
+        "lua", "luadoc", "luap",
+        "markdown", "markdown_inline",
+        "python", "regex",
+        "toml", "tsx", "typescript",
+        "vim", "vimdoc", "yaml",
       },
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-
-      vim.defer_fn(function()
-        pcall(vim.cmd, "TSUpdate")
-      end, 0)
-    end
   },
-})
+}
