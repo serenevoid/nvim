47375c58c2d97631509efddb04525c63ca754674
P H Aju <aju.ph@exorint.com>
2026-03-16 08:00:52 +0530

    fix(telescope): remove version anchor

--- lua/void/plugins/telescope.lua
+++ lua/void/plugins/telescope.lua
@@ -6,7 +6,6 @@ local default_settings = {
 
 return {
   'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-fzf-native.nvim'
